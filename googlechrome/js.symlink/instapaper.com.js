cd9e16bd7d7f434257058a9f62588e9824facb8a
Donnie Ray Jones <donnierayjones@gmail.com>
2012-06-03 11:14:24 -0500

    added .js and .css files

---
+++ googlechrome/js.symlink/instapaper.com.js
@@ -0,0 +1,1 @@
+$('.tableViewCellTitleLink').attr('target', '_blank');
---
+++ googlechrome/js.symlink/read.amazon.com.js
@@ -0,0 +1,10 @@
+//$(function() {
+  //var kindleReaderBookContainer = $('#kindleReader_book_container');
+  //kindleReaderBookContainer.bind('DOMSubtreeModified', function() {
+    //var frames = $('iframe', kindleReaderBookContainer);
+    //var styles = '<style type="text/css">#a\\:0 { -webkit-column-count: 2; }</style>';
+    //$.each(frames, function(i, f) {
+      //$(styles).appendTo($('head', f.contentDocument));
+    //});
+  //});
+//});
